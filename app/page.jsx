53a019376db6c131ff330a1540cf97e1e6bd4312
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-05-11 17:25:55 +0530

    add sidebar.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,6 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1 className="font-bold text-4xl text-purple-300">Hello world this is from hintro dashboard</h1>
       <Sidebar />
     </div>
   );
